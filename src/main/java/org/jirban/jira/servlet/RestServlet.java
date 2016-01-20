8c6419cf09cd6b492ac97b72e813bbd690d0b275
Kabir Khan <kkhan@redhat.com>
2016-01-20 17:05:18 +0000

    Servlet can inject simple component

--- src/main/java/org/jirban/jira/servlet/RestServlet.java
+++ src/main/java/org/jirban/jira/servlet/RestServlet.java
@@ -1,17 +1,24 @@
 package org.jirban.jira.servlet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
 
-import javax.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.jirban.jira.api.MyPluginComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RestServlet extends HttpServlet{
     private static final Logger log = LoggerFactory.getLogger(RestServlet.class);
 
+    private final MyPluginComponent myPluginComponent;
+
+    public RestServlet(MyPluginComponent myPluginComponent) {
+        this.myPluginComponent = myPluginComponent;
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
