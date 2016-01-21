fa056b23544c372ba27a4001ce06d75fa6ce7b65
Kabir Khan <kkhan@redhat.com>
2016-01-21 15:05:23 +0000

    Initial template to load resources from webapp directory

--- src/main/java/org/jirban/jira/servlet/InitServlet.java
+++ src/main/java/org/jirban/jira/servlet/InitServlet.java
@@ -1,6 +1,8 @@
 package org.jirban.jira.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -18,28 +20,39 @@ import com.atlassian.jira.config.properties.ApplicationProperties;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.templaterenderer.TemplateRenderer;
 
+/**
+ * Loads up the init.vm template which drives the angular app, initialised with links to the correct directories.
+ */
 @Named("jirbanInitServlet")
 public class InitServlet extends HttpServlet{
     private static final Logger log = LoggerFactory.getLogger(InitServlet.class);
 
+    private static final String INIT_BROWSER_TEMPLATE = "/templates/init.vm";
+
     @ComponentImport
     private final TemplateRenderer templateRenderer;
+
+    private final ApplicationProperties applicationProperties;
 
     @Inject
     public InitServlet(final TemplateRenderer templateRenderer) {
         this.templateRenderer = templateRenderer;
+        this.applicationProperties = ComponentAccessor.getApplicationProperties();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ApplicationProperties applicationProperties = ComponentAccessor.getApplicationProperties();
-        String baseUrl = applicationProperties.getString(APKeys.JIRA_BASEURL);
-        System.out.println("Base url: " + baseUrl);
+        System.out.println("Initialising the template");
 
-        //webResourceManager.requireResource("jirban-jira-resources.jirban-jira.webapp");
-        resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Init Servlet " +
-                "</body></html>");
+        StringBuilder webappRoot = new StringBuilder(applicationProperties.getString(APKeys.JIRA_BASEURL));
+        if (webappRoot.charAt(webappRoot.length() - 1) != '/') {
+            webappRoot.append('/');
+        }
+        webappRoot.append("download/resources/org.jirban.jirban-jira/webapp");
+
+        Map<String, Object> context = new HashMap<>();
+        context.put("webappRoot", webappRoot.toString());
+        templateRenderer.render(INIT_BROWSER_TEMPLATE, context, resp.getWriter());
     }
 
 }
