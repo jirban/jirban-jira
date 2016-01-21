5444e85109092597e3c4031d2a62c0f6b8f8a142
Kabir Khan <kkhan@redhat.com>
2016-01-21 09:41:51 +0000

    Make issue event listener register/deregister itself

--- src/main/java/org/jirban/jira/impl/JirbanIssueEventListener.java
+++ src/main/java/org/jirban/jira/impl/JirbanIssueEventListener.java
@@ -21,32 +21,79 @@
  */
 package org.jirban.jira.impl;
 
-import com.atlassian.jira.event.issue.AbstractIssueEventListener;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.atlassian.event.api.EventListener;
+import com.atlassian.event.api.EventPublisher;
 import com.atlassian.jira.event.issue.IssueEvent;
+import com.atlassian.jira.event.type.EventType;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 
 /**
- * The listener listening to issue events, and calling back to the state table.
- * This must be registered with Jira as mentioned in
- * <a href="https://confluence.atlassian.com/jira/listeners-185729466.html">https://confluence.atlassian.com/jira/listeners-185729466.html</a>
+ * The listener listening to issue events, and delegating relevant ones to the issue table.
  *
  * @author Kabir Khan
  */
-public class JirbanIssueEventListener extends AbstractIssueEventListener {
-    @Override
-    protected void handleDefaultIssueEvent(IssueEvent event) {
-        System.out.println("---> Default issue event: " + event);
-        super.handleDefaultIssueEvent(event);
+@Named("jirbanIssueEventListener")
+public class JirbanIssueEventListener  implements InitializingBean, DisposableBean {
+    private static final Logger log = LoggerFactory.getLogger(JirbanIssueEventListener.class);
+
+    @ComponentImport
+    private final EventPublisher eventPublisher;
+
+    /**
+     * Constructor.
+     * @param eventPublisher injected {@code EventPublisher} implementation.
+     */
+    @Autowired
+    public JirbanIssueEventListener(EventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
     }
 
-    @Override
-    public void workflowEvent(IssueEvent event) {
-        System.out.println("---> Workflow event: " + event);
-        super.workflowEvent(event);
+    /**
+     * Called when the plugin has been enabled.
+     * @throws Exception
+     */
+    public void afterPropertiesSet() throws Exception {
+        // register ourselves with the EventPublisher
+        System.out.println("-----> Registering listener");
+        eventPublisher.register(this);
     }
 
-    @Override
-    public void customEvent(IssueEvent event) {
-        System.out.println("---> Custom event: " + event);
-        super.customEvent(event);
+    /**
+     * Called when the plugin is being disabled or removed.
+     * @throws Exception
+     */
+    public void destroy() throws Exception {
+        // unregister ourselves with the EventPublisher
+        System.out.println("-----> Unregistering listener");
+        eventPublisher.unregister(this);
     }
+
+    /**
+     * Receives any {@code IssueEvent}s sent by JIRA.
+     * @param issueEvent the IssueEvent passed to us
+     */
+    @EventListener
+    public void onIssueEvent(IssueEvent issueEvent) {
+        Long eventTypeId = issueEvent.getEventTypeId();
+        Issue issue = issueEvent.getIssue();
+        // if it's an event we're interested in, log it
+        System.out.println("-----> Event " + issueEvent);
+        if (eventTypeId.equals(EventType.ISSUE_CREATED_ID)) {
+            log.info("Issue {} has been created at {}.", issue.getKey(), issue.getCreated());
+        } else if (eventTypeId.equals(EventType.ISSUE_RESOLVED_ID)) {
+            log.info("Issue {} has been resolved at {}.", issue.getKey(), issue.getResolutionDate());
+        } else if (eventTypeId.equals(EventType.ISSUE_CLOSED_ID)) {
+            log.info("Issue {} has been closed at {}.", issue.getKey(), issue.getUpdated());
+        }
+    }
+
 }
