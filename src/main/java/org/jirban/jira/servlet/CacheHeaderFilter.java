d630936259ab05b58222aba9db8afa07c32c1a18
Kabir Khan <kkhan@redhat.com>
2016-09-09 18:09:49 +0100

    [#39] Since webpack gives us a unique hash when the app is rebuilt, set a longer cache expiry. ETag isn't really relevant any longer

--- src/main/java/org/jirban/jira/servlet/CacheHeaderFilter.java
+++ src/main/java/org/jirban/jira/servlet/CacheHeaderFilter.java
@@ -1,6 +1,7 @@
 package org.jirban.jira.servlet;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -17,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 public class CacheHeaderFilter implements Filter {
 
     private final String etagHex = Long.toHexString(System.currentTimeMillis());
-    private static final int MAX_AGE = 60 * 60 * 6; //6 hours max age
+    private static final long MAX_AGE = TimeUnit.DAYS.toSeconds(30); //6 hours max age
 
     public CacheHeaderFilter() {
     }
@@ -28,16 +29,20 @@ public class CacheHeaderFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse resp = (HttpServletResponse)response;
         String ifNoneMatch = req.getHeader("if-none-match");
-
-        if (etagHex.equals(ifNoneMatch)) {
-            ((HttpServletResponse) response).setStatus(304);
+        if (ifNoneMatch != null) {
+            //If this header is there, it means the browser has it cached. Webpack gives all the js and other files
+            //a unique hash
+            if (!etagHex.equals(ifNoneMatch)) {
+                //The etag is different so update it for the client (not really necessary).
+                resp.addHeader("ETag", etagHex);
+            }
+            resp.setStatus(304);
             return;
         }
 
-        HttpServletResponse resp = (HttpServletResponse)response;
         resp.addHeader("ETag", etagHex);
         resp.addHeader("Cache-Control", "max-age=" + MAX_AGE);
         chain.doFilter(request, resp);
