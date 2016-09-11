15cb178437734d40ecb750cffaff6f3d418900d9
Kabir Khan <kkhan@redhat.com>
2016-09-11 11:42:09 +0100

    [#39] Get tests working via karma
    Former-commit-id: 4ac329448bb2163a0691c6ea9d8f82f536d36196

--- webapp/config/webpack.test.js
+++ webapp/config/webpack.test.js
@@ -24,12 +24,12 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
+        exclude: helpers.root('app'),
         loader: 'null'
       },
       {
         test: /\.css$/,
-        include: helpers.root('src', 'app'),
+        include: helpers.root('app'),
         loader: 'raw'
       }
     ]
