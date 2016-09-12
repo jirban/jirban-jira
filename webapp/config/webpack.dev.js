86ec3d356358d0fc4b12335495f3406a6c1efa2e
Kabir Khan <kkhan@redhat.com>
2016-09-12 17:32:21 +0100

    [#39] Calculate the webpack public path on runtime.
    This gets rid of a huge worry I had about how to make the public path
    start with '/jira' when running in the SDK, not start with '/jira' when
    running in the public instance (where the root context is used), and to
    use simply the url of the dev server for dev mode. :)

--- webapp/config/webpack.dev.js
+++ webapp/config/webpack.dev.js
@@ -6,9 +6,13 @@ var helpers = require('./helpers');
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
 
+  /**
+   * Note that publicPath is not set here at build time since we don't know exactly where the war will be deployed. boot.js sets
+   * the global __webpack_public_path__ which then calculates the path dynamically.
+   */
   output: {
     path: helpers.root('dist'),
-    publicPath: 'http://localhost:3000/',
+    //publicPath: 'http://localhost:3000/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
