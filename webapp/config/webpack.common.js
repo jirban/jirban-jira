6b3bc9f81ec5714c188f83f613e6e180f913b7f6
Kabir Khan <kkhan@redhat.com>
2016-11-19 14:39:24 +0000

    Rename webpack helpers and get rid of default webpack.config.js

--- webapp/config/webpack.common.js
+++ webapp/config/webpack.common.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var helpers = require('./helpers');
+var helpers = require('./webpack.helpers');
 
 module.exports = {
   entry: {
--- webapp/config/webpack.dev.js
+++ webapp/config/webpack.dev.js
@@ -1,7 +1,7 @@
 var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
-var helpers = require('./helpers');
+var helpers = require('./webpack.helpers');
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
