62b9f854120eaf1fb74c053e19e6ddcab194a0bd
Kabir Khan <kkhan@redhat.com>
2017-01-27 12:16:41 +0000

    Fix compile error

--- webapp/app/data/board/tests/assignee.spec.ts
+++ webapp/app/data/board/tests/assignee.spec.ts
@@ -4,27 +4,27 @@ describe('Assignee tests', ()=> {
     describe('Initials', () => {
 
         it('4 word name', () => {
-            let assignee:Assignee = new Assignee("a", "a@a.com", "http://a.com", "Kabir Middle Khan Ignored");
+            let assignee:Assignee = new Assignee("a", "a", "a@a.com", "http://a.com", "Kabir Middle Khan Ignored");
             expect(assignee.initials).toEqual("KMK");
         });
 
         it('3 word name', () => {
-            let assignee:Assignee = new Assignee("a", "a@a.com", "http://a.com", "Kabir Middle Khan");
+            let assignee:Assignee = new Assignee("a", "a", "a@a.com", "http://a.com", "Kabir Middle Khan");
             expect(assignee.initials).toEqual("KMK");
         });
 
         it('2 word name', () => {
-            let assignee:Assignee = new Assignee("a", "a@a.com", "http://a.com", "Kabir Khan");
+            let assignee:Assignee = new Assignee("a", "a", "a@a.com", "http://a.com", "Kabir Khan");
             expect(assignee.initials).toEqual("KK");
         });
 
         it ('1 word name - lower case', () => {
-            let assignee:Assignee = new Assignee("a", "a@a.com", "http://a.com", "admin");
+            let assignee:Assignee = new Assignee("a", "a", "a@a.com", "http://a.com", "admin");
             expect(assignee.initials).toEqual("Adm");
         });
 
         it ('1 word name - upper case', () => {
-            let assignee:Assignee = new Assignee("a", "a@a.com", "http://a.com", "ADMIN");
+            let assignee:Assignee = new Assignee("a", "a", "a@a.com", "http://a.com", "ADMIN");
             expect(assignee.initials).toEqual("Adm");
         });
     });
